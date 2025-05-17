f012614126683aec85e67df0679fe5dfa2a2f334
MAC <mac@MACs-MacBook-Pro.local>
2025-05-17 12:20:46 +0530

    established the connection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express= require("express");
 const app= express();
-
+const connection = require("../config/db");
 app.use("/",(req,res)=>{
 res.send("Hello World");
 })
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
-}
-);
+
+const PORT=process.env.PORT || 5000;
+connection().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+    console.log("Database connected successfully");
+}).catch((err)=>{
+    console.log("Database connection failed",err);
+})
+
